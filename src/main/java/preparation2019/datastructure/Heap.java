afee662a1a4abd5ef2ef677d59e0b57e47dd1465
denisn26 <denis.tisov@n26.com>
2019-05-28 17:16:01 +0200

    heap

--- src/main/java/preparation2019/datastructure/Heap.java
+++ src/main/java/preparation2019/datastructure/Heap.java
@@ -13,27 +13,65 @@ public class Heap<T extends Comparable<T>> {
         }
     }
 
-    public void siftUp(int index) {
-
+    public static <T extends Comparable<T>> Heap<T> buildHeap(List<T> array) {
+        return new Heap<>(array);
     }
 
-    public void siftDown(int index) {
+    public void push(T element) {
+        if (element == null) {
+            throw new IllegalArgumentException();
+        }
+        heap.add(element);
+        siftUp(heap.size() - 1);
+    }
+
+    public T poll() {
+        if (heap.isEmpty()) {
+            return null;
+        }
+
+        T result = heap.get(0);
+        int lastElementIndex = heap.size() - 1;
+        heap.set(0, heap.get(lastElementIndex));
+        heap.remove(lastElementIndex);
+
+        if (!heap.isEmpty()) {
+            siftDown(0);
+        }
+        return result;
+    }
+
+    private void siftUp(int index) {
+        int parentIndex = (index - 1) / 2;
+        while (index > 0) {
+            if (heap.get(parentIndex).compareTo(heap.get(index)) < 0) {
+                swap(parentIndex, index);
+                index = parentIndex;
+                parentIndex = (index - 1) / 2;
+            } else {
+                return;
+            }
+        }
+    }
+
+
+    private void siftDown(int index) {
         int leftChildIndex = 2 * index + 1;
         int rightChildIndex = 2 * index + 2;
         T indexValue = heap.get(index);
 
         int maxChildIndex;
-        if (leftChildIndex < heap.size() && rightChildIndex < heap.size()) {
+        if (exists(rightChildIndex)) {
             T leftChildValue = heap.get(leftChildIndex);
             T rightChildValue = heap.get(rightChildIndex);
-            if (leftChildValue.compareTo(rightChildValue) >= 0) {
+            if (leftChildValue.compareTo(rightChildValue) > 0) {
                 maxChildIndex = leftChildIndex;
             } else if (rightChildValue.compareTo(leftChildValue) > 0) {
                 maxChildIndex = rightChildIndex;
             } else {
                 return;
             }
-        } else if (leftChildIndex < heap.size() && heap.get(leftChildIndex).compareTo(indexValue) >= 0) {
+        } else if (exists(leftChildIndex) && heap.get(leftChildIndex).compareTo(indexValue) >= 0) {
             maxChildIndex = leftChildIndex;
         } else {
             return;
@@ -43,14 +81,15 @@ public class Heap<T extends Comparable<T>> {
         siftDown(maxChildIndex);
     }
 
+    private boolean exists(int index) {
+        return index < heap.size();
+    }
+
+
     private void swap(int firstElement, int secondElement) {
         T firstElementValue = heap.get(firstElement);
         heap.set(firstElement, heap.get(secondElement));
         heap.set(secondElement, firstElementValue);
-    }
-
-    public static <T extends Comparable<T>> Heap buildHeap(List<T> array) {
-        return new Heap<>(array);
     }
 
     @Override
--- src/test/java/preparation2019/datastructure/HeapTest.java
+++ src/test/java/preparation2019/datastructure/HeapTest.java
@@ -11,9 +11,41 @@ import static preparation2019.datastructure.Heap.buildHeap;
 public class HeapTest {
 
     @Test
-    public void testBuildHeap() {
-        Heap heap = buildHeap(new ArrayList<>(Arrays.asList(3, 4, 5, 6, 7, 8)));
+    public void testBuildHeap1() {
+        Heap<Integer> heap = buildHeap(new ArrayList<>(Arrays.asList(3, 4, 5, 6, 7, 8)));
 
-        Assert.assertEquals(heap.heap, Arrays.asList(8, 7, 5, 6, 4, 3));
+        Assert.assertEquals(Arrays.asList(8, 7, 5, 6, 4, 3), heap.heap);
+    }
+
+    @Test
+    public void testBuildHeap2() {
+        Heap<Integer> heap = buildHeap(new ArrayList<>(Arrays.asList(3, 4, 6, 5, 6, 7, 8, 3)));
+
+        Assert.assertEquals(Arrays.asList(8, 6, 7, 5, 4, 3, 6, 3), heap.heap);
+    }
+
+    @Test
+    public void testPoll() {
+        Heap<Integer> heap = buildHeap(new ArrayList<>(Arrays.asList(3, 4, 5, 6, 7, 8)));
+
+        Assert.assertEquals(8, heap.poll().intValue());
+        Assert.assertEquals(7, heap.poll().intValue());
+        Assert.assertEquals(6, heap.poll().intValue());
+        Assert.assertEquals(5, heap.poll().intValue());
+        Assert.assertEquals(4, heap.poll().intValue());
+        Assert.assertEquals(3, heap.poll().intValue());
+        Assert.assertNull(heap.poll());
+        Assert.assertNull(heap.poll());
+    }
+
+    @Test
+    public void testPush() {
+        Heap<Integer> heap = buildHeap(new ArrayList<>(Arrays.asList(3, 4, 5)));
+
+        heap.push(6);
+        heap.push(8);
+        heap.push(7);
+
+        Assert.assertEquals(Arrays.asList(8, 6, 7, 4, 5, 3), heap.heap);
     }
 }
